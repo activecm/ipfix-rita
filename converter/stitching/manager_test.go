a18ed75a7309fe2f0a180a7add43c2d062a3e1c7
Ankit <dev.ang1@enbake.com>
2018-07-05 18:48:47 +0530

    Local code pushed with the functions for udp.

--- converter/stitching/manager_test.go
+++ converter/stitching/manager_test.go
@@ -125,6 +125,64 @@ func requireFlowStitchedWithZeroes(t *testing.T, flow ipfix.Flow, sess *session.
 	}
 }
 
+func requireFlowStitchedWithNoZeroes(t *testing.T, flow ipfix.Flow, sess *session.Aggregate) {
+	// Todo : Needs to update based on No Zeroes understanding
+	sourceIsA := flow.SourceIPAddress() < flow.DestinationIPAddress()
+	if sourceIsA {
+		require.Equal(t, flow.Exporter(), sess.Exporter)
+		require.Equal(t, flow.ProtocolIdentifier(), sess.ProtocolIdentifier)
+
+		//ensure Source -> Dest information matches A -> B
+		require.Equal(t, flow.SourceIPAddress(), sess.IPAddressA)
+		require.Equal(t, flow.DestinationIPAddress(), sess.IPAddressB)
+		require.Equal(t, flow.SourcePort(), sess.PortA)
+		require.Equal(t, flow.DestinationPort(), sess.PortB)
+		require.Equal(t, flow.OctetTotalCount(), sess.OctetTotalCountAB)
+		require.Equal(t, flow.PacketTotalCount(), sess.PacketTotalCountAB)
+		require.Equal(t, flow.FlowEndReason(), sess.FlowEndReasonAB)
+
+		startTime, err := flow.FlowStartMilliseconds()
+		require.Nil(t, err)
+		endTime, err := flow.FlowEndMilliseconds()
+		require.Nil(t, err)
+		require.Equal(t, startTime, sess.FlowStartMillisecondsAB)
+		require.Equal(t, endTime, sess.FlowEndMillisecondsAB)
+
+		//ensure B -> A information is Zero/ Nil
+		require.Zero(t, sess.OctetTotalCountBA)
+		require.Zero(t, sess.PacketTotalCountBA)
+		require.Zero(t, sess.FlowStartMillisecondsBA)
+		require.Zero(t, sess.FlowEndMillisecondsBA)
+		require.Equal(t, ipfix.Nil, sess.FlowEndReasonBA)
+	} else {
+		require.Equal(t, flow.Exporter(), sess.Exporter)
+		require.Equal(t, flow.ProtocolIdentifier(), sess.ProtocolIdentifier)
+
+		//ensure Source -> Dest information matches B -> A
+		require.Equal(t, flow.SourceIPAddress(), sess.IPAddressB)
+		require.Equal(t, flow.DestinationIPAddress(), sess.IPAddressA)
+		require.Equal(t, flow.SourcePort(), sess.PortB)
+		require.Equal(t, flow.DestinationPort(), sess.PortA)
+		require.Equal(t, flow.OctetTotalCount(), sess.OctetTotalCountBA)
+		require.Equal(t, flow.PacketTotalCount(), sess.PacketTotalCountBA)
+		require.Equal(t, flow.FlowEndReason(), sess.FlowEndReasonBA)
+
+		startTime, err := flow.FlowStartMilliseconds()
+		require.Nil(t, err)
+		endTime, err := flow.FlowEndMilliseconds()
+		require.Nil(t, err)
+		require.Equal(t, startTime, sess.FlowStartMillisecondsBA)
+		require.Equal(t, endTime, sess.FlowEndMillisecondsBA)
+
+		//ensure A -> B information is Zero/ Nil
+		require.Zero(t, sess.OctetTotalCountAB)
+		require.Zero(t, sess.PacketTotalCountAB)
+		require.Zero(t, sess.FlowStartMillisecondsAB)
+		require.Zero(t, sess.FlowEndMillisecondsAB)
+		require.Equal(t, ipfix.Nil, sess.FlowEndReasonAB)
+	}
+}
+
 func TestGoRoutineLeaks(t *testing.T) {
 	numGoRoutines := runtime.NumGoroutine()
 
@@ -396,6 +454,55 @@ func TestTwoUPDFlowFlippedSource(t *testing.T){
 
 	requireFlowStitchedWithZeroes(t, flow1, sessions[0])
 	requireFlowStitchedWithZeroes(t, flow2, sessions[1])
+
+	timeOut1 := sessions[0].FlowEndMillisecondsBA + milliseconds
+	timeOut2 := sessions[1].FlowStartMillisecondsAB
+
+	if timeOut2 >= timeOut1 {
+		t.Fatalf("The difference between the flowEnd of the first connection and the flowStart of the next must be less than the sameSessionTimeout")
+	}
+}
+
+func TestTwoUPDFlowOneAggregateZero(t *testing.T){
+	//Todo : Needs to understand the condition to identify if its one aggreate only with two udp flow.
+	env, cleanup := environmenttest.SetupIntegrationTest(t)
+	defer cleanup()
+
+	flow1 := ipfix.NewFlowMock()
+	flow1.MockSourceIPAddress = "1.1.1.1"
+	flow1.MockSourcePort = 0
+	flow1.MockDestinationIPAddress = "2.2.2.2"
+	flow1.MockDestinationPort = 771
+	flow1.MockProtocolIdentifier = protocols.UDP
+	flow1.MockFlowEndReason = ipfix.IdleTimeout
+
+	flow2 := ipfix.NewFlowMock()
+	flow2.MockSourceIPAddress = flow1.MockSourceIPAddress
+	flow2.MockDestinationIPAddress = flow1.MockDestinationIPAddress
+	flow2.MockSourcePort = flow1.MockSourcePort
+	flow2.MockDestinationPort = flow1.MockDestinationPort
+	flow2.MockExporter = flow1.MockExporter
+	flow2.MockProtocolIdentifier = flow1.MockProtocolIdentifier
+	flow2.MockFlowEndReason = flow1.MockFlowEndReason
+	flow2.MockFlowStartMilliseconds = flow1.MockFlowEndMilliseconds + 100
+	flow2.MockFlowEndMilliseconds = flow2.MockFlowStartMilliseconds + (flow1.MockFlowEndMilliseconds - flow1.MockFlowStartMilliseconds)
+
+	stitchingManager := newTestingStitchingManager()
+	sessions, errs := stitchingManager.RunSync([]ipfix.Flow{flow1, flow2}, env.DB)
+
+	//ensure 1 aggregate is created since its UDP
+	require.Len(t, sessions, 1)
+
+	//ensure there were no errors
+	if len(errs) != 0 {
+		for i := range errs {
+			t.Error(errs[i])
+		}
+	}
+	require.Len(t, errs, 0)
+
+	requireFlowStitchedWithZeroes(t, flow1, sessions[0])
+	//requireFlowStitchedWithZeroes(t, flow2, sessions[1])
 
 	timeOut1 := sessions[0].FlowEndMillisecondsBA + milliseconds
 	timeOut2 := sessions[1].FlowStartMillisecondsAB
