01c4c8c711957c1bbb04887be2e20d8e0236d61a
Logan Lembke <logan@blackhillsinfosec.com>
2018-09-11 16:04:06 -0600

    add back in log statement

--- converter/output/rita/streaming/dates/rita_dates.go
+++ converter/output/rita/streaming/dates/rita_dates.go
@@ -279,9 +279,9 @@ WriteLoop:
 				//Insert into yesterday's db
 				s.previousCollection.Insert(ritaConn)
 			} else {
-				/*s.log.Info("dropping out-of-time-segment session", logging.Fields{
+				s.log.Info("dropping out-of-time-segment session", logging.Fields{
 					"session": fmt.Sprintf("%+v", sess),
-				})*/
+				})
 				//TODO: Add counters and track this
 				//Drop the connection record
 			}
