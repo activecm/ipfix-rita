ea01e150852b70108c50bf1d44a8ec15536436e8
Samuel Carroll <sam2@blackhillsinfosec.com>
2019-01-08 12:00:07 -0700

    Work in progress, I will still need to test this but good groundwork for breaking out IPFIX-RITA's installer to something nicer

---
+++ install-scripts/helpers/check_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit values
+# 0 correct version installed
+# 3 not installed
+# 4 older than required minimum version
+# 5 newer than required maximum version
+
+if [ ! -x "$(command -v docker)" ]; then
+	exit 3
+fi
+
+MIN_VERSION_MAJOR=17
+MIN_VERSION_MINOR=05
+MAX_VERSION_MAJOR=18
+MAX_VERSION_MINOR=09
+
+VERSION="$(docker -v | sed 's/^.* \([0-9][0-9]*\)\.\([0-9][0-9]*\)\.\([0-9][0-9]*\).*$/\1 \2 \3/')"
+VERSION_MAJOR="$(echo $VERSION | cut -d' ' -f1)"
+VERSION_MINOR="$(echo $VERSION | cut -d' ' -f2)"
+
+if [ "$VERSION_MAJOR" -lt "$MIN_VERSION_MAJOR" ] ||
+	[ "$VERSION_MAJOR" -eq "$MIN_VERSION_MAJOR" -a "$VERSION_MINOR" -lt "$MIN_VERSION_MINOR" ]; then
+	exit 4
+elif [ "$VERSION_MAJOR" -gt "$MAX_VERSION_MAJOR" ] ||
+	[ "$VERSION_MAJOR" -eq "$MAX_VERSION_MAJOR" -a "$VERSION_MINOR" -gt "$MAX_VERSION_MINOR" ]; then
+	exit 5
+else
+	exit 0
+fi
