1b1721ab277f85be6c64af055697b2f6ed83a3ee
Logan Lembke <logan@blackhillsinfosec.com>
2019-01-24 16:12:41 -0700

    Add SafeMap to input. Did not integrate.

---
+++ converter/input/logstash/data/safemap/data.go
@@ -0,0 +1,30 @@
+package safemap
+
+import "github.com/globalsign/mgo/bson"
+
+//stringMap returns arbitrary data given a field name.
+//stringMap is used to abstract over map[string]interface{}
+//and bson.M via MapStringMap and BsonStringMap respectively.
+type stringMap interface {
+	//Get retreives arbitrary data for a given field name
+	Get(field string) (interface{}, bool)
+}
+
+//mapStringMap adapts map[string]interface{} objects
+//to StringMap objects
+type mapStringMap map[string]interface{}
+
+//Get retreives arbitrary data for a given field name
+func (m mapStringMap) Get(field string) (interface{}, bool) {
+	val, ok := m[field]
+	return val, ok
+}
+
+//bsonStringMap adapts bson.M objects to StringMap objects
+type bsonStringMap bson.M
+
+//Get retreives arbitrary data for a given field name
+func (b bsonStringMap) Get(field string) (interface{}, bool) {
+	val, ok := b[field]
+	return val, ok
+}
---
+++ converter/input/logstash/data/safemap/untyped_map.go
@@ -0,0 +1,128 @@
+package safemap
+
+import "gopkg.in/mgo.v2/bson"
+import "github.com/pkg/errors"
+
+var (
+	//ErrDoesNotExist notes that there is not a field with the given name in the SafeMap
+	ErrDoesNotExist = errors.New("field does not exist in SafeMap")
+	//ErrTypeMismatch notes that there is not a field with the given name and type in the SafeMap
+	ErrTypeMismatch = errors.New("value returned for given field does not match requested type")
+)
+
+//SafeMap provides convenience methods for safely accessing
+//data in map[string]interface objects.
+type SafeMap struct {
+	innerMap stringMap
+}
+
+//NewSafeMapFromBSON adapts a bson.M object to a SafeMap
+func NewSafeMapFromBSON(bsonMap bson.M) SafeMap {
+	return SafeMap{
+		innerMap: bsonStringMap(bsonMap),
+	}
+}
+
+//NewSafeMap adapts a map[string]interface{} to a SafeMap
+func NewSafeMap(data map[string]interface{}) SafeMap {
+	return SafeMap{
+		innerMap: mapStringMap(data),
+	}
+}
+
+func (u SafeMap) get(field string) (interface{}, error) {
+	val, ok := u.innerMap.Get(field)
+	if !ok {
+		return nil, ErrDoesNotExist
+	}
+	return val, nil
+}
+
+//GetSafeMap returns a SafeMap backed by the inner map
+//held in the given field
+func (u SafeMap) GetSafeMap(field string) (SafeMap, error) {
+	dataIface, err := u.get(field)
+	if err != nil {
+		return SafeMap{}, err
+	}
+
+	//Right now we only support bson.M and map[string]interface{}
+	//for backing SafeMaps. Try both.
+	rawMap, ok := dataIface.(map[string]interface{})
+	if ok {
+		return NewSafeMap(rawMap), nil
+	}
+
+	bsonMap, ok := dataIface.(bson.M)
+	if ok {
+		return NewSafeMapFromBSON(bsonMap), nil
+	}
+
+	return SafeMap{}, errors.Wrap(
+		ErrTypeMismatch,
+		"Field: "+field+"; Type: map[string]interface{}/ bson.M",
+	)
+}
+
+//GetInt returns the int value for a given field.
+//If the field does not exist or the data matching the field is not the
+//appropriate type, an error is returned.
+func (u SafeMap) GetInt(field string) (int, error) {
+	val, err := u.get(field)
+	if err != nil {
+		return 0, errors.Wrap(ErrDoesNotExist, "Field: "+field)
+	}
+
+	intVal, ok := val.(int)
+	if ok {
+		return intVal, nil
+	}
+	return 0, errors.Wrap(
+		ErrTypeMismatch,
+		"Field: "+field+"; Type: int",
+	)
+}
+
+//GetIntAsInt64 returns the int64 value for a given field.
+//If the data matching the field is of type int, a cast will be performed.
+//If the field does not exist or the data matching the field is not the
+//appropriate type, an error is returned.
+func (u SafeMap) GetIntAsInt64(field string) (int64, error) {
+	val, err := u.get(field)
+	if err != nil {
+		return 0, errors.Wrap(ErrDoesNotExist, "Field: "+field)
+	}
+
+	int64Val, ok := val.(int64)
+	if ok {
+		return int64Val, nil
+	}
+
+	intVal, ok := val.(int)
+	if ok {
+		return int64(intVal), nil
+	}
+	return 0, errors.Wrap(
+		ErrTypeMismatch,
+		"Field: "+field+"; Type: int64/ int",
+	)
+}
+
+//GetString returns the string value for a given field.
+//If the field does not exist or the data matching the field is not the
+//appropriate type, an error is returned.
+func (u SafeMap) GetString(field string) (string, error) {
+	val, err := u.get(field)
+	if err != nil {
+		return "", errors.Wrap(ErrDoesNotExist, "Field: "+field)
+	}
+
+	strVal, ok := val.(string)
+	if ok {
+		return strVal, nil
+	}
+	return "", errors.Wrap(
+		ErrTypeMismatch,
+		"Field: "+field+"; Type: string",
+	)
+}
